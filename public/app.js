a5b4f424c7e78991f702d24bb0098620420145c0
Fernando Briano <fernando@picandocodigo.net>
2014-03-09 14:07:05 -0300

    Refactor for CSS and JS in their own files

---
+++ public/app.js
@@ -0,0 +1,40 @@
+function loadXMLDoc(command){
+  var xmlhttp;
+
+  if(window.XMLHttpRequest){
+    xmlhttp = new XMLHttpRequest();
+  }
+
+  xmlhttp.onreadystatechange = function(){
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+      setVolume( JSON.parse(xmlhttp.responseText) );
+    }
+  }
+
+  xmlhttp.open("GET", "/" + command, true);
+  xmlhttp.send();
+}
+
+function setVolume(data){
+  document.getElementById("volume").innerHTML = data.number;
+  document.getElementById("state").innerHTML = data.state;
+}
+
+document.getElementById("up").addEventListener("click", up);
+document.getElementById("down").addEventListener("click", down);
+document.getElementById("mute").addEventListener("click", mute);
+
+function up(){
+  loadXMLDoc("volup");
+  return false;
+}
+
+function down(){
+  loadXMLDoc("voldown");
+  return false;
+}
+
+function mute(){
+  loadXMLDoc("mute");
+  return false;
+}
